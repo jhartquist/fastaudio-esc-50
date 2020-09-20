9e5e97e5ff2f81838535a4ed608b02188c49f464
John Hartquist <john@hartquist.com>
2020-09-20 14:22:43 -0700

    sweep_4

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from fastai.vision.all import * 
 from fastaudio.core.all import *
+from fastaudio.augment.all import *
 
 # from https://github.com/fastaudio/Audio-Competition/blob/master/ESC-50-baseline-1Fold.ipynb
 def CrossValidationSplitter(col='fold', fold=1):
@@ -29,3 +30,28 @@ class StackedMelSpecs(Transform):
 
     def encodes(self, x: AudioTensor) -> AudioSpectrogram:       
         return AudioSpectrogram(torch.cat([spec(x) for spec in self.specs], axis=1))
+    
+def pitch_shift(t: AudioTensor, n_steps):
+    assert t.shape[0] == 1
+    x = t.numpy()[0]
+    y = librosa.effects.pitch_shift(x, t.sr, n_steps)
+    t.data = torch.from_numpy(y[np.newaxis, :])
+    return t
+
+class PitchShifter(RandTransform):
+    """Randomly shifts the audio signal by `max_pct` %.
+        direction must be -1(left) 0(bidirectional) or 1(right).
+    """
+
+    def __init__(self, max_steps=3):
+        super().__init__()
+        self.max_steps = max_steps
+
+    def before_call(self, b, split_idx):
+        super().before_call(b, split_idx)
+        self.shift_factor = random.uniform(-1, 1)
+
+    def encodes(self, t: AudioTensor):
+        n_steps = self.shift_factor * self.max_steps
+        t = pitch_shift(t, n_steps)
+        return t
